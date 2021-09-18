2c6efe74cfb62d9337cbcd40d0d8c6ed83ccaa63
Khalid <58151955+HikaruSadashi@users.noreply.github.com>
2021-09-18 17:32:17 -0400

    Didnt save

---
+++ distance.py
@@ -0,0 +1,4 @@
+# Class of functions and objects that 
+# TAKE: a destination; like Lions Arena
+# her column for how far they are from the inputted destination (in dataframe) 
+# you would have to check how far it is somehow using a library
---
+++ main.py
@@ -0,0 +1,6 @@
+# import the dataset and pandas library
+# remove all columns except the ones mentioned on trello
+# add the vacant or not column to the dataframe
+# append the imported unsorted column of distance values (from distance.py file)
+# sort the whole dataframe based on lowest (closest) distance values to biggest ones
+# return that dataframe but only with the columns mentioned on trello
