2ca19e02ca6128c6df7bf4bef8e3d97c7a99e0c2
Khalid Zabalawi <58151955+HikaruSadashi@users.noreply.github.com>
2021-09-18 23:15:11 +0000

    Prepped Data

--- main.py
+++ main.py
@@ -4,3 +4,4 @@
 # append the imported unsorted column of distance values (from distance.py file)
 # sort the whole dataframe based on lowest (closest) distance values to biggest ones
 # return that dataframe but only with the columns mentioned on trello
+
